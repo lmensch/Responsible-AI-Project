8cb4fb05aa1d90204b80a1c974500dbd20da7dc8
Leon Mensch <59626523+lmensch@users.noreply.github.com>
2025-07-16 15:02:09 +0200

    Benchmark whole dataset

--- dataset_attention.py
+++ dataset_attention.py
@@ -392,8 +392,8 @@ ds_idx = []
 results = []
 reasons = []
 feature_rows = []
-start_idx = int(0)
-end_idx = int(10)  #len(train_ds)
+start_idx = int(100)
+end_idx = int(400)  #len(train_ds)
 total = int(end_idx-start_idx)
 pbar = tqdm.tqdm(total=total)
 print(f"Benchmark: {start_idx} - {end_idx}")
